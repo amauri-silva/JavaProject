1d65a43e4a6075a3c96c18668b0cb41a68bc6bd8
Amauri Silva <amauribiarge@gmail.com>
2023-08-16 20:57:49 -0300

    The initial project commit

---
+++ jevent/src/main/java/br/com/jevent/JeventApplication.java
@@ -0,0 +1,13 @@
+package br.com.jevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeventApplication.class, args);
+	}
+
+}
