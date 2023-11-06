502710f9cb5502f3e80db5637ffafcacd5524dc0
Amauri Silva <amauribiarge@gmail.com>
2023-11-06 18:39:49 -0300

    Project core base refactoring

--- Event-driven/coinevent/src/main/java/br/com/coinevent/dto/RequestUser.java
+++
@@ -1,6 +0,0 @@
-package main.java.br.com.coinevent.dto;
-
-public class RequestUser {
-    private String userId;
-    private String coingWindowSize;
-}
--- Event-driven/coinevent/src/main/java/br/com/coinevent/service/CoinStreaminService.java
+++
@@ -1,9 +0,0 @@
-package main.java.br.com.coinevent.service;
-
-import main.java.br.com.coinevent.dto.RequestUser;
-
-public interface CoinStreaminService {
-
-    void callProducer(final RequestUser request);
-    
-}
